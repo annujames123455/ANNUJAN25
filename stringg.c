6fee2016c09b5c090272a0e979b764cafa0acf63
annujames <annujames2028@cs.sjcetpalai.ac.in>
2025-01-22 16:25:40 +0530

    Add files via upload

---
+++ stringg.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+printf("size of integer =%ld",sizeof(int));
+printf("\nsize of long integer =%ld",sizeof(long int));
+printf("\nsize of shot integer=%ld",sizeof(short int));
+printf("\nsize of Float=%ld",sizeof(float));
+printf("\nsize of Double=%ld",sizeof(double));
+
+return 0;
+}
