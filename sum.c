25cfe0620337e9b0c27a4ce15666e655ed4a548e
annujames <annujames2028@cs.sjcetpalai.ac.in>
2025-01-22 16:23:56 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+ int main(){
+ 
+     float number1,number2;
+     float sum;
+     printf("\n enter 2 float numbers");
+     scanf("%f%f",&number1,&number2);
+ sum=number1+number2;
+ printf("%f+%f=%f\n", number1,number2,sum);
+ 
+ 
+ return 0;
+ }
+    
